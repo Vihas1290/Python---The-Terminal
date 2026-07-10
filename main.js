b762197db47c52172b7514c71c6d3f73d576ae24
Vihas1290 <itchgdevpro@gmail.com>
2026-07-10 21:06:29 +0400

    X

--- main.js
+++
@@ -1,7 +0,0 @@
-function revealLocation() {
-  const input = document.getElementById("input").value;
-  const output = document.getElementById("output");
-  output.innerHTML = `Your location will be leaked! Have fun (while it lasts). ik its not ${input}.`;
-}
-
-document.getElementById("submit").addEventListener("click", revealLocation);
